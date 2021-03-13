65ec5f844673b782f860b7e3aa06b508f8369ba7
David Mason <masond@ufl.edu>
2021-03-13 09:49:19 -0500

    finished hw

--- hw1.R
+++ hw1.R
@@ -5,6 +5,7 @@ library(emmeans) # Calculating and comparing means
 library(DataExplorer) # Data exploration
 library(lme4) # Modeling 
 library(lmerTest) # Testing random effects
+library(MuMIn) # R squared value
 
 d <- read.csv("data.csv") # Load in the data
 
@@ -75,14 +76,43 @@ anova(lm(Time~Hand*Person, data = d)) # Hand:Person isn't significant
 # be a random effect anyway. 
 
 # Model with log10-transformed values without nesting hand
-m4 <- lmer(log10(Time)~Type*Hand+(1|Person), d) 
+m4 <- (lmer(log10(Time)~Type*Hand+(1|Person), d))
 print(VarCorr(m4), comp=c("Variance")) 
 
 # Statistical test on base10
+summary(m4)
 anova(m4)
 emmeans(m4, pairwise~Hand|Type) 
+emmeans(m4, pairwise~Hand) 
+r.squaredGLMM(m4) 
 # Difference between foraging rates is 
 # significant for penny and not for bean 
 
-
 # Figures ####
+d_means <- as.data.frame(emmeans(m4, pairwise~Hand|Type)) 
+# turn emmeans output into data frame
+d_means <- d_means[1:4,] # drop the interaction terms
+d_means$dummy <- c('Bean_Primary', 'Bean_Secondary',  # create a dummy variable 
+									 'Penny_Primary', 'Penny_Secondary')  # for plotting
+d_means$dummy <- factor(d_means$dummy, levels = c("Bean_Primary", 
+										"Penny_Primary", "Bean_Secondary", "Penny_Secondary"))
+
+c(6, 10, 16, 25)
+
+ggplot(d = d_means, aes(x = dummy, y = emmean, fill = Type))+
+	geom_errorbar(aes(ymin = lower.CL, ymax = upper.CL), size = 2, width = 0,
+                color = "gray31")+
+	geom_point(size = 15, shape = 21)+
+	ylab('Foraging rate (sec)')+
+  xlab('Predator attack hand')+
+	scale_fill_grey(start = 0.7, end = 1, name = "Prey Type")+
+	theme_classic()+
+	scale_x_discrete(labels=c("Bean_Primary" = "Primary", "Penny_Primary" = "",
+               "Bean_Secondary" = "Secondary","Penny_Secondary" = ""))+
+	scale_y_continuous(labels=c("0.8" = "6", "1.0" = "10",
+                              "1.2" = "16", "1.4" = "25"))+
+	theme(text = element_text(size = 25), 
+				axis.text.x = element_text(hjust = -0.0),
+				axis.ticks.x = element_blank())+
+	theme(axis.title.x = element_text(vjust=-0.5))+
+	theme(axis.title.x = element_text(hjust=6))
